e651cd437e6a30bbd76bb793c7e19249be05501d
shifu-dev <csingh8476@gmail.com>
2023-02-15 05:45:59 +0530

    [FEAT, WIP]: Add Internal::LogTargetBase and ConsoleLogTarget.

---
+++ Source/Logging/Public/Atom/Logging/LogTargets/ConsoleLogTarget.h
@@ -0,0 +1,95 @@
+#pragma once
+#include <stddef.h>
+#include "Atom/Logging/LogTargets/LogTargetBase.h"
+
+namespace Atom::Logging
+{
+    /// --------------------------------------------------------------------------------------------
+    /// ConsoleLogTarget logs to console using stdout and stderr.
+    /// --------------------------------------------------------------------------------------------
+    class ConsoleLogTarget : public Internal::LogTargetBase
+    {
+    public:
+        /// ----------------------------------------------------------------------------------------
+        /// Default constructs the ConsoleLogTarget.
+        /// 
+        /// ErrorLogLevel is set to LogLevel::Error.
+        /// ----------------------------------------------------------------------------------------
+        ConsoleLogTarget() noexcept:
+            _stdout(stdout), _stderr(stderr),
+            _errLogLevel(LogLevel::Error) { }
+
+    public:
+        /// ----------------------------------------------------------------------------------------
+        /// Retuns the LogLevel specified for error logging.
+        /// ----------------------------------------------------------------------------------------
+        LogLevel GetErrLogLevel() const noexcept
+        {
+            return _errLogLevel;
+        }
+
+        /// ----------------------------------------------------------------------------------------
+        /// Sets the LogLevel specified for error logging.
+        /// 
+        /// Logs of same or above level are written to stderr.
+        /// ----------------------------------------------------------------------------------------
+        void SetErrLogLevel(LogLevel lvl) noexcept
+        {
+            _errLogLevel = lvl;
+        }
+
+        /// ----------------------------------------------------------------------------------------
+        /// Checks if LogLevel should be written to stderr.
+        /// 
+        /// @RETURNS {true} if {lvl >= GetErrLogLevel()}, else {false}.
+        /// ----------------------------------------------------------------------------------------
+        bool ShouldLogAsError(LogLevel lvl) const noexcept
+        {
+            return lvl >= _errLogLevel;
+        }
+
+    protected:
+        /// ----------------------------------------------------------------------------------------
+        /// Writes the formatted message to stdout or stderr based on log level.
+        /// 
+        /// If {ShouldLogAsError(logMsg.lvl) == true}, writes the message to stderr else to stdout.
+        /// ----------------------------------------------------------------------------------------
+        virtual void _Write(const LogMsg& logMsg, StringView formattedMsg) override final
+        {
+            FILE* file = _stdout;
+
+            if (ShouldLogAsError(logMsg.lvl))
+            {
+                file = _stderr;
+            }
+
+            ::fwrite(formattedMsg.data(), sizeof(char), formattedMsg.size(), file);
+        }
+
+        /// ----------------------------------------------------------------------------------------
+        /// Flushes both stdout and stderr.
+        /// ----------------------------------------------------------------------------------------
+        virtual void _Flush() override final
+        {
+            fflush(_stdout);
+            fflush(_stderr);
+        }
+
+    protected:
+
+        /// ----------------------------------------------------------------------------------------
+        /// FILE* to stdout.
+        /// ----------------------------------------------------------------------------------------
+        FILE* _stdout;
+
+        /// ----------------------------------------------------------------------------------------
+        /// FILE* to stderr.
+        /// ----------------------------------------------------------------------------------------
+        FILE* _stderr;
+
+        /// ----------------------------------------------------------------------------------------
+        /// ErrorLogLevel used to check if the message should be logged to stderr or stdout.
+        /// ----------------------------------------------------------------------------------------
+        LogLevel _errLogLevel;
+    };
+}
---
+++ Source/Logging/Public/Atom/Logging/LogTargets/LogTargetBase.h
@@ -0,0 +1,180 @@
+#pragma once
+#include "fmt/core.h"
+#include "Atom/Logging/LogTarget.h"
+
+namespace Atom::Logging::Internal
+{
+    /// --------------------------------------------------------------------------------------------
+    /// Base class for LogTargets with base functionality like formatting, thread safety and
+    /// level filtering.
+    /// 
+    /// @TODO Add thread safety.
+    /// @TODO Make default log and flush level global.
+    /// --------------------------------------------------------------------------------------------
+    class LogTargetBase: public LogTarget
+    {
+    public:
+        /// ----------------------------------------------------------------------------------------
+        /// Default constructs LogTargetBase().
+        /// ----------------------------------------------------------------------------------------
+        LogTargetBase() noexcept:
+            _logLevel(LogLevel::Debug), _flushLevel(LogLevel::Info),
+            _hasWritten(false), _alwaysFlush(false) { }
+
+    //// -------------------------------------------------------------------------------------------
+    //// API Functions
+    //// -------------------------------------------------------------------------------------------
+
+    public:
+        /// ----------------------------------------------------------------------------------------
+        /// Filters and Formats the LogMsg and passes it to write.
+        /// 
+        /// @EXCEPTION_SAFETY Strong.
+        /// 
+        /// @SEE _Write().
+        /// ----------------------------------------------------------------------------------------
+        virtual void Write(const LogMsg& logMsg) override final
+        {
+            if (CheckLogLevel(logMsg.lvl))
+            {
+                // TODO: Fix this compilation error.
+                // String result = fmt::format("[{TIME}] [{LEVEL}] {LOGGER_NAME}: {MSG}",
+                //     logMsg.time, logMsg.lvl, logMsg.loggerName, logMsg.msg);
+
+                String result;
+
+                _hasWritten = true;
+                _Write(logMsg, result);
+
+                if (CheckFlushLevel(logMsg.lvl))
+                {
+                    _Flush();
+                }
+            }
+        }
+
+        /// ----------------------------------------------------------------------------------------
+        /// Flushes if {ShouldFlush() == true}.
+        /// 
+        /// @EXCEPTION_SAFETY Strong.
+        /// ----------------------------------------------------------------------------------------
+        virtual void Flush() override final
+        {
+            if (ShouldFlush())
+            {
+                _Flush();
+            }
+        }
+
+        /// ----------------------------------------------------------------------------------------
+        /// Get the log level.
+        /// ----------------------------------------------------------------------------------------
+        LogLevel GetLogLevel() const noexcept
+        {
+            return _logLevel;
+        }
+
+        /// ----------------------------------------------------------------------------------------
+        /// Sets the log level.
+        /// ----------------------------------------------------------------------------------------
+        void SetLogLevel(LogLevel lvl) noexcept
+        {
+            _logLevel = lvl;
+        }
+
+        /// ----------------------------------------------------------------------------------------
+        /// Checks if we should log the message of specified level.
+        /// ----------------------------------------------------------------------------------------
+        bool CheckLogLevel(LogLevel lvl) const noexcept
+        {
+            if (lvl == LogLevel::OFF) return false;
+            if (lvl < _logLevel) return false;
+
+            return true;
+        }
+
+        /// ----------------------------------------------------------------------------------------
+        /// Gets the flush level.
+        /// ----------------------------------------------------------------------------------------
+        LogLevel GetFlushLevel() const noexcept
+        {
+            return _flushLevel;
+        }
+
+        /// ----------------------------------------------------------------------------------------
+        /// Sets the flush level.
+        /// ----------------------------------------------------------------------------------------
+        void SetFlushLevel(LogLevel lvl) noexcept
+        {
+            _flushLevel = lvl;
+        }
+
+        /// ----------------------------------------------------------------------------------------
+        /// Checks if should flush after logging the message of specified level.
+        /// It also asks ShouldFlush().
+        /// ----------------------------------------------------------------------------------------
+        bool CheckFlushLevel(LogLevel lvl) const noexcept
+        {
+            if (!_hasWritten) return false;
+            if (lvl == LogLevel::OFF) return false;
+            if (lvl < _flushLevel) return false;
+
+            return true;
+        }
+
+        /// ----------------------------------------------------------------------------------------
+        /// Checks if we should flush.
+        /// 
+        /// @RETURNS {true} if there has been a log since last flush, else {false}.
+        /// ----------------------------------------------------------------------------------------
+        bool ShouldFlush() const noexcept
+        {
+            return _alwaysFlush || _hasWritten;
+        }
+
+    //// -------------------------------------------------------------------------------------------
+    //// Implementation Functions
+    //// -------------------------------------------------------------------------------------------
+
+    protected:
+        /// ----------------------------------------------------------------------------------------
+        /// Write implementation.
+        /// 
+        /// @PARAM[IN] logMsg Log message object passed for logging.
+        /// @PARAM[IN] formattedMsg Formatted message generated from {logMsg}.
+        /// ----------------------------------------------------------------------------------------
+        virtual void _Write(const LogMsg& logMsg, StringView formattedMsg) = 0;
+
+        /// ----------------------------------------------------------------------------------------
+        /// Flush implementation.
+        /// ----------------------------------------------------------------------------------------
+        virtual void _Flush() = 0;
+
+    //// -------------------------------------------------------------------------------------------
+    //// Variables
+    //// -------------------------------------------------------------------------------------------
+
+    protected:
+        /// ----------------------------------------------------------------------------------------
+        /// Log Level used to filter log messages.
+        /// ----------------------------------------------------------------------------------------
+        LogLevel _logLevel;
+
+        /// ----------------------------------------------------------------------------------------
+        /// Flush level used to check if to call flush after logging.
+        /// ----------------------------------------------------------------------------------------
+        LogLevel _flushLevel;
+
+        /// ----------------------------------------------------------------------------------------
+        /// Value used to check if there has been any write since last flush.
+        /// ----------------------------------------------------------------------------------------
+        bool _hasWritten;
+
+        /// ----------------------------------------------------------------------------------------
+        /// If true always calls underlying flush _Flush(), even if not necessary.
+        /// This doesn't override CheckFlushLevel(LogLevel lvl) check. It only affects 
+        /// ShouldFlush().
+        /// ----------------------------------------------------------------------------------------
+        bool _alwaysFlush;
+    };
+}
