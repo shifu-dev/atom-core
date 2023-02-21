859f7ed5aa749e6941aad19496b02ef4b4cd5019
shifu-dev <csingh8476@gmail.com>
2023-02-21 19:05:50 +0530

    [TESTS] Added Atom::Logging tests.

---
+++ Source/Logging/Tests/TestLog.cpp
@@ -0,0 +1,30 @@
+#include "catch2/catch_all.hpp"
+#include "Atom/Logging/Log.h"
+
+using namespace Atom::Logging;
+
+TEST_CASE("STATIC_LOG", "[Implementation]")
+{
+    BENCHMARK("LOG")
+    {
+        LOG_TRACE("HOLLA! This is an error.");
+        return 0;
+    };
+
+    BENCHMARK("STATIC_LOG - Optimized")
+    {
+        STATIC_LOG_TRACE("HOLLA! This is trace.");
+        return 0;
+    };
+
+    BENCHMARK("STATIC_LOG - NotOptimized")
+    {
+        STATIC_LOG_ERROR("HOLLA! This is error.");
+        return 0;
+    };
+
+    BENCHMARK("EMPTY")
+    {
+        return 0;
+    };
+}
