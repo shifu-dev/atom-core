10aa69c39e51757261a540365112b0884a671bf7
shifu-dev <csingh8476@gmail.com>
2023-02-21 21:16:12 +0530

    [REFACTOR] Refactored code.

--- Source/Core/Public/Atom/String/String.h
+++ Source/Core/Public/Atom/String/String.h
@@ -1,11 +1,9 @@
 #pragma once
 #include <string>
-#include <string_view>
 
 #include "Atom/Core.h"
 
 namespace Atom
 {
     using String = std::string;
-    using StringView = std::string_view;
 }
---
+++ Source/Core/Public/Atom/String/StringView.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <string_view>
+
+#include "Atom/Core.h"
+
+namespace Atom
+{
+    using StringView = std::string_view;
+}
