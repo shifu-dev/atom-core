694000d58fd84974ec6f4188b2b663afb6479c87
shifu-dev <csingh8476@gmail.com>
2023-03-01 22:47:45 +0530

    [REFACTOR] Refactored code.

--- Source/Core/Public/Atom/Core.h
+++ Source/Core/Public/Atom/Core.h
@@ -20,8 +20,7 @@
     #endif
 
 #else
-    #define ATOM_API
-    #error "AtomEngine only supports Windows and Linux platform."
+    #error "AtomEngine only supports Windows and Linux platform for now."
 
 #endif
 
