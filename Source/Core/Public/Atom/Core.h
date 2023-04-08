0bbcf36d26b98fb17ba58ab9ff859351ea4c6de9
Chetan Singh <csingh8476@gmail.com>
2023-04-08 15:52:43 +0530

    [FEAT] Added alias to primitive types.

--- Source/Core/Public/Atom/Core.h
+++ Source/Core/Public/Atom/Core.h
@@ -57,6 +57,20 @@
 namespace Atom
 {
     using byte = unsigned char;
+
+    using uchar = unsigned char;
+
+    using int8 = int8_t;
+    using int16 = int16_t;
+    using int32 = int32_t;
+    using int64 = int64_t;
+
+    using uint8 = uint8_t;
+    using uint16 = uint16_t;
+    using uint32 = uint32_t;
+    using uint64 = uint64_t;
+    using uint = unsigned int;
+
     using usize = std::size_t;
     using isize = std::ptrdiff_t;
 
