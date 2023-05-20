42f035e59872c4dd61f43dfe54e6f553c0f14867
Chetan Singh <csingh8476@gmail.com>
2023-05-20 18:55:25 +0530

    [FEAT] Add Atom.Range wrapper for iter.

---
+++ Source/Core/Public/Atom/Containers/Range.h
@@ -0,0 +1,43 @@
+#pragma once
+#include "Ranges.h"
+
+namespace Atom
+{
+    template <typename TInIter, typename TInIterEnd>
+    struct Range
+    {
+    public:
+        using TIter = TInIter;
+        using TIterEnd = TInIterEnd;
+        using TEnd = TInIterEnd;
+
+    public:
+        constexpr Range(TIter begin, TIterEnd end) noexcept:
+            _begin{ begin }, _end{ end } { }
+
+    public:
+        constexpr TIter Begin() const noexcept
+        {
+            return _begin;
+        }
+
+        constexpr TIterEnd End() const noexcept
+        {
+            return _end;
+        }
+
+        constexpr TIter begin() const noexcept
+        {
+            return Begin();
+        }
+
+        constexpr TIterEnd end() const noexcept
+        {
+            return End();
+        }
+
+    protected:
+        TIter _begin;
+        TIterEnd _end;
+    };
+}
