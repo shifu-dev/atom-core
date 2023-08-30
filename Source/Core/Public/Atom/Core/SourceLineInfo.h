8988fb532b7ca011bb7e324dcc8fda08ebc431d0
Chetan Singh <csingh8476@gmail.com>
2023-08-30 08:01:13 +0530

    [FIX] Workaround not support for SourceLineInfo in clang.

--- Source/Core/Public/Atom/Core/SourceLineInfo.h
+++ Source/Core/Public/Atom/Core/SourceLineInfo.h
@@ -9,31 +9,47 @@ namespace Atom
     /// --------------------------------------------------------------------------------------------
     class SourceLineInfo
     {
-    private:
-        using _TImpl = std::source_location;
-
     public:
         /// --------------------------------------------------------------------------------------------
         /// Get the source line info of the calling site.
-        /// 
+        ///
         /// # Note
         /// - This default argument is used to capture the source line info of the calling site.
         ///   Don't pass any argument for this.
         /// --------------------------------------------------------------------------------------------
-        static constexpr SourceLineInfo current(_TImpl src = _TImpl::current())
+#if defined(ATOM_COMP_CLANG) && !defined(__cpp_lib_source_location)
+        static consteval auto current(
+            const char *fileName = __builtin_FILE(),
+            const char *funcName = __builtin_FUNCTION(),
+            u32 line = __builtin_LINE(),
+            u32 column = __builtin_COLUMN()) -> SourceLineInfo
         {
-            return SourceLineInfo {
-                .line = src.line(),
-                .column = src.column(),
-                .fileName = src.file_name(),
-                .funcName = src.function_name()
+            return SourceLineInfo
+            {
+                .line = line,
+                .column = column,
+                .funcName = funcName,
+                .fileName = fileName
             };
         }
+#else
+        static consteval auto current(std::source_location src = 
+            std::source_location::current()) -> SourceLineInfo
+        {
+            return SourceLineInfo
+            {
+                .line = src.line(),
+                .column = src.column(),
+                .funcName = src.function_name(),
+                .fileName = src.file_name()
+            };
+        }
+#endif
 
     public:
         u32 line;
         u32 column;
-        const char* fileName;
         const char* funcName;
+        const char* fileName;
     };
 }
