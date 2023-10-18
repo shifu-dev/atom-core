ea4b710c296f5ceec61ee8a4919a0be87d2d6414
shifu-dev <csingh8476@gmail.com>
2023-10-18 12:11:35 +0530

    [Refactor] Clean Sandbox.

--- Source/Core/Sandbox/Sandbox.cpp
+++ Source/Core/Sandbox/Sandbox.cpp
@@ -1,7 +1,4 @@
 #include "Atom/Core.h"
-#include "Atom/Str/StackStr.h"
-
-#include <string>
 
 using namespace Atom;
 
@@ -16,9 +13,6 @@ auto PrintCmdArgs(i32 argc, char** argv)
 auto main(int argc, char** argv) -> int
 {
     PrintCmdArgs(argc, argv);
-
-    i32 num;
-    auto str = num.toStr();
 
     return 0;
 }
