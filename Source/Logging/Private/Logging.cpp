1ce108e3d1111543cdc65d310a7609e3ea273ad7
shifu-dev <csingh8476@gmail.com>
2023-10-21 13:44:05 +0530

    [UPDATE] Remove Atom.Engine and Atom.Logging.

--- Source/Logging/Private/Logging.cpp
+++
@@ -1,5 +0,0 @@
-#include "Atom/Logging/LogTarget.h"
-#include "Atom/Logging/LogTargets/ConsoleLogTarget.h"
-#include "Atom/Logging/Logger.h"
-#include "Atom/Logging/LoggerRegistry.h"
-#include "Atom/Logging/NullLogger.h"
