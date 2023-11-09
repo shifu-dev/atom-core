9320e36e649b4e4608b10f05e0dd0d9c5db041f1
shifu-dev <csingh8476@gmail.com>
2023-11-09 22:47:21 +0530

    refactor: remove old code from sandbox.

--- Source/Sandbox/Sandbox.cpp
+++ Source/Sandbox/Sandbox.cpp
@@ -1,5 +1,4 @@
-// #include "Atom/CoreAll.h"
-#include "Atom/Memory/MemPtr.h"
+#include "Atom/CoreAll.h"
 #include <iostream>
 
 using namespace Atom;
@@ -15,95 +14,6 @@ auto PrintCmdArgs(i32 argc, char** argv)
 auto main(int argc, char** argv) -> int
 {
     PrintCmdArgs(argc, argv);
-
-    {
-        ConstPtr<void> cvoidptr;
-        ConstPtr<int> cintptr;
-
-        Ptr<void> voidptr;
-        Ptr<int> intptr;
-
-        ConstPtr<int> cintptr0 = (const int*)nullptr;
-        ConstPtr<int> cintptr1 = (int*)nullptr;
-        ConstPtr<int> cintptr2 = cvoidptr;
-        ConstPtr<int> cintptr3 = voidptr;
-
-        cintptr0 = (const int*)nullptr;
-        cintptr1 = (int*)nullptr;
-        cintptr2 = cvoidptr;
-        cintptr3 = voidptr;
-
-        Ptr<int> intptr0 = (int*)nullptr;
-        Ptr<int> intptr1 = voidptr;
-
-        intptr0 = (int*)nullptr;
-        intptr1 = voidptr;
-
-        ConstPtr<void> cvoidptr0 = cintptr;
-        ConstPtr<void> cvoidptr1 = intptr;
-
-        cvoidptr0 = cintptr;
-        cvoidptr1 = intptr;
-
-        Ptr<void> voidptr0 = intptr;
-
-        voidptr0 = intptr;
-    }
-
-    {
-        ConstPtr<void> constPtrVoid;
-        ConstPtr<int> constPtrInt;
-        Ptr<void> ptrVoid;
-        Ptr<int> ptrInt;
-
-        ConstMemPtr<void> constMemPtrVoid;
-        ConstMemPtr<int> constMemPtrInt;
-        MemPtr<void> memPtrVoid;
-        MemPtr<int> memPtrInt;
-
-        ConstPtr<void> constPtrVoid0 = constMemPtrVoid;
-        ConstPtr<int> constPtrInt0 = constMemPtrInt;
-        Ptr<void> ptrVoid0 = memPtrVoid;
-        Ptr<int> ptrInt0 = memPtrInt;
-    }
-
-    {
-        ConstMemPtr<void> cvoidptr;
-        ConstMemPtr<int> cintptr;
-
-        MemPtr<void> voidptr;
-        MemPtr<int> intptr;
-
-        ConstMemPtr<int> cintptr0 = (const int*)nullptr;
-        ConstMemPtr<int> cintptr1 = (int*)nullptr;
-        ConstMemPtr<int> cintptr2 = cvoidptr;
-        ConstMemPtr<int> cintptr3 = voidptr;
-
-        cintptr0 = (const int*)nullptr;
-        cintptr1 = (int*)nullptr;
-        cintptr2 = cvoidptr;
-        cintptr3 = voidptr;
-
-        MemPtr<int> intptr1 = (int*)nullptr;
-        MemPtr<int> intptr3 = voidptr;
-
-        intptr1 = (int*)nullptr;
-        intptr3 = voidptr;
-
-        ConstMemPtr<void> cvoidptr0 = cintptr;
-        ConstMemPtr<void> cvoidptr1 = intptr;
-
-        cvoidptr0 = cintptr;
-        cvoidptr1 = intptr;
-    }
-
-    {
-        ConstMemPtr<int> constmemptrint = ConstMemPtr<void>();
-        ConstMemPtr<void> constmemptrvoid = ConstMemPtr<int>();
-
-        MemPtr<int> memptrint = MemPtr<void>();
-        MemPtr<void> memptrvoid = MemPtr<int>();
-    }
 
     return 0;
 }
