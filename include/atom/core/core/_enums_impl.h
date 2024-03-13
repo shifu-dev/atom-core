0b811dc4821381dda2a41e6656cc577ee95b348e
shifu-dev <csingh8476@gmail.com>
2024-03-13 10:36:42 +0530

    update: add enums impl.

---
+++ include/atom/core/core/_enums_impl.h
@@ -0,0 +1,218 @@
+#pragma once
+#include "atom/core/string/string_view.h"
+#include "atom/core/core/tuple.h"
+#include "atom/core/core/option.h"
+#include "atom/core/math.h"
+#include "magic_enum_all.hpp"
+#include <array>
+#include <utility>
+
+namespace atom
+{
+    template <typename enum_t>
+    class _enums_impl
+    {
+        using magic_string_t = magic_enum::string;
+        using magic_string_view_t = magic_enum::string_view;
+
+        template <typename value_t>
+        using magic_optional_t = magic_enum::optional<value_t>;
+
+        template <typename first_value_t, typename second_value_t>
+        using magic_pair_t = std::pair<first_value_t, second_value_t>;
+
+        template <typename value_t, usize count>
+        using magic_array_t = std::array<value_t, count>;
+
+    public:
+        using underlying_t = magic_enum::underlying_type_t<enum_t>;
+
+        static consteval auto is_enum() -> bool
+        {
+            return typeinfo::is_enum<enum_t>;
+        }
+
+        static consteval auto is_scoped() -> bool
+        {
+            return magic_enum::is_scoped_enum_v<enum_t>;
+        }
+
+        static constexpr auto from_string(string_view str) -> option<enum_t>
+        {
+            auto result = magic_enum::enum_cast<enum_t>(str.to_std());
+            if (not result.has_value())
+                return nullopt();
+
+            return string_view::from_std(result.value());
+        }
+
+        template <typename comparer_t>
+        static constexpr auto from_string(string_view str, comparer_t&& comparer) -> option<enum_t>
+        {
+            auto result =
+                magic_enum::enum_cast<enum_t>(str.to_std(), forward<comparer_t>(comparer));
+            if (not result.has_value())
+                return nullopt();
+
+            return string_view::from_std(result.value());
+        }
+
+        static constexpr auto from_underlying_unchecked(underlying_t value) -> enum_t
+        {
+            return enum_t(value);
+        }
+
+        static constexpr auto from_underlying_try(underlying_t value) -> option<enum_t>
+        {
+            magic_optional_t value_opt = magic_enum::enum_cast<enum_t>(value);
+            if (not value_opt.has_value())
+                return nullopt();
+
+            return value_opt.value();
+        }
+
+        static constexpr auto from_index_unchecked(usize index) -> enum_t
+        {
+            return magic_enum::enum_value<enum_t>(index);
+        }
+
+        static constexpr auto from_index_try(usize index) -> option<enum_t>
+        {
+            if (index >= get_count())
+                return nullopt();
+
+            return magic_enum::enum_value<enum_t>(index);
+        }
+
+        static consteval auto get_type_name() -> string_view
+        {
+            return string_view::from_std(magic_enum::enum_type_name<enum_t>());
+        }
+
+        static consteval auto get_count() -> usize
+        {
+            return magic_enum::enum_count<enum_t>();
+        }
+
+        static consteval auto get_values() -> array_view<enum_t>
+        {
+            auto values = magic_enum::enum_values<enum_t>();
+            return array_view<enum_t>(values.data(), values.size());
+        }
+
+        static consteval auto get_strings() -> array_view<string_view>
+        {
+            magic_array_t<magic_string_view_t, get_count()> magic_strings =
+                magic_enum::enum_names<enum_t>();
+
+            magic_array_t<string_view, get_count()> strings;
+            for (usize i = 0; i < get_count(); i++)
+            {
+                strings[i] = string_view::from_std(magic_strings[i]);
+            }
+
+            return array_view<string_view>(strings.data(), strings.size());
+        }
+
+        static consteval auto get_entries() -> array_view<tuple<enum_t, string_view>>
+        {
+            magic_array_t<magic_pair_t<enum_t, magic_string_view_t>, get_count()> magic_strings =
+                magic_enum::enum_names<enum_t>();
+
+            magic_array_t<tuple<enum_t, string_view>, get_count()> strings;
+            for (usize i = 0; i < get_count(); i++)
+            {
+                strings[i] = tuple<enum_t, string_view>(
+                    magic_strings[i].first, string_view::from_std(magic_strings[i].second));
+            }
+
+            return array_view<tuple<enum_t, string_view>>(strings.data(), strings.size());
+        }
+
+        static constexpr auto is_value_valid(enum_t value) -> bool
+        {
+            return magic_enum::enum_contains(value);
+        }
+
+        static constexpr auto is_index_valid(usize index) -> bool
+        {
+            return index < get_count();
+        }
+
+        static constexpr auto is_underlying_valid(underlying_t value) -> bool
+        {
+            return magic_enum::enum_contains(value);
+        }
+
+        static constexpr auto to_index(enum_t value) -> usize
+        {
+            return magic_enum::enum_index(value).value_or(math::max<usize>());
+        }
+
+        static constexpr auto to_underlying(enum_t value) -> underlying_t
+        {
+            return underlying_t(value);
+        }
+
+        static constexpr auto to_string_view(enum_t value) -> string_view
+        {
+            return string_view::from_std(magic_enum::enum_name(value));
+        }
+
+        /// @todo implement this.
+        static consteval auto get_min() -> enum_t {}
+
+        /// @todo implement this.
+        static consteval auto get_max() -> enum_t {}
+
+        /// @todo implement this. this is just a workaround.
+        static consteval auto get_all_flags() -> enum_t
+        {
+            return enum_t(math::max<underlying_t>());
+        }
+
+        static constexpr auto add_flags(enum_t lhs, enum_t rhs) -> enum_t
+        {
+            return enum_t(underlying_t(lhs) | underlying_t(rhs));
+        }
+
+        static constexpr auto remove_flags(enum_t lhs, enum_t rhs) -> enum_t
+        {
+            return enum_t(underlying_t(lhs) & ~underlying_t(rhs));
+        }
+
+        static constexpr auto get_common_flags(enum_t lhs, enum_t rhs) -> enum_t
+        {
+            return enum_t(underlying_t(lhs) & underlying_t(rhs));
+        }
+
+        static constexpr auto get_uncommon_flags(enum_t lhs, enum_t rhs) -> enum_t
+        {
+            underlying_t union_flags = underlying_t(lhs) | underlying_t(rhs);
+            underlying_t intersection_flags = underlying_t(lhs) & underlying_t(rhs);
+            return enum_t(union_flags & intersection_flags);
+        }
+
+        static constexpr auto get_reverse_flags(enum_t flags) -> enum_t
+        {
+            return enum_t(underlying_t(get_all_flags()) & ~underlying_t(flags));
+        }
+
+        static constexpr auto has_all_flags(enum_t lhs, enum_t rhs) -> bool
+        {
+            return (underlying_t(lhs) & underlying_t(rhs)) == underlying_t(lhs);
+        }
+
+        static constexpr auto has_any_flags(enum_t lhs, enum_t rhs) -> bool
+        {
+            return (underlying_t(lhs) & underlying_t(rhs)) != 0;
+        }
+    };
+}
+
+template <typename enum_type>
+    requires atom::_enums_impl<enum_type>::is_enum
+struct magic_enum::customize::enum_range<enum_type>
+{
+    static constexpr bool is_flags = atom::_enums_impl<enum_type>::is_flags();
+};
--- include/atom/core/core/enums.h
+++ include/atom/core/core/enums.h
@@ -1,30 +1,26 @@
 #pragma once
-#include "atom/core/containers/array_view.h"
-#include "atom/core/string/string_view.h"
-#include "atom/core/core/option.h"
-#include "atom/core/typeinfo.h"
-#include "atom/core/core/tuple.h"
-#include "magic_enum.hpp"
+#include "_enums_impl.h"
 
 namespace atom::enums
 {
     /// --------------------------------------------------------------------------------------------
-    /// returns the underlying data type used for storage.
+    /// returns the underlying data type used for storage of enum, if the type is not enum,
+    /// compiler error occurs.
     /// --------------------------------------------------------------------------------------------
     template <typename enum_type>
-    using get_underlying_type = magic_enum::underlying_type_t<enum_type>;
+    using get_underlying_type = _enums_impl<enum_type>::underlying_t;
 
     /// --------------------------------------------------------------------------------------------
     /// returns `true` if `enum_type` is an enum.
     /// --------------------------------------------------------------------------------------------
     template <typename enum_type>
-    constexpr bool is_enum = typeinfo::is_enum<enum_type>;
+    constexpr bool is_enum = _enums_impl<enum_type>::is_enum();
 
     /// --------------------------------------------------------------------------------------------
     /// returns `true` if enum is a scoped enum.
     /// --------------------------------------------------------------------------------------------
     template <typename enum_type>
-    constexpr bool is_scoped = false;
+    constexpr bool is_scoped = _enums_impl<enum_type>::is_scoped();
 
     /// --------------------------------------------------------------------------------------------
     /// returns true if enum type is flags.
@@ -64,7 +60,10 @@ namespace atom::enums
     /// --------------------------------------------------------------------------------------------
     template <typename enum_type>
     constexpr auto from_string(string_view str) -> option<enum_type>
-        requires is_enum<enum_type>;
+        requires is_enum<enum_type>
+    {
+        return _enums_impl<enum_type>::from_string(str);
+    }
 
     /// --------------------------------------------------------------------------------------------
     /// returns first enum value of type `enum_type` matching string `str`. same as above, except
@@ -74,114 +73,159 @@ namespace atom::enums
     /// --------------------------------------------------------------------------------------------
     template <typename enum_type, typename comparer_type>
     constexpr auto from_string(string_view str, comparer_type&& comparer) -> option<enum_type>
-        requires is_enum<enum_type>;
+        requires is_enum<enum_type>
+    {
+        return _enums_impl<enum_type>::from_string(str, ATOM_FORWARD(comparer));
+    }
 
     /// --------------------------------------------------------------------------------------------
     /// returns enum value of type `enum_type` created from underlying type.
     /// --------------------------------------------------------------------------------------------
     template <typename enum_type>
     constexpr auto from_underlying_unchecked(get_underlying_type<enum_type> value) -> enum_type
-        requires is_enum<enum_type>;
+        requires is_enum<enum_type>
+    {
+        return _enums_impl<enum_type>::from_underlying_unchecked(value);
+    }
 
     /// --------------------------------------------------------------------------------------------
     /// returns enum value of type `enum_type` created from underlying type.
     /// --------------------------------------------------------------------------------------------
     template <typename enum_type>
     constexpr auto from_underlying_try(get_underlying_type<enum_type> value) -> option<enum_type>
-        requires is_enum<enum_type>;
+        requires is_enum<enum_type>
+    {
+        return _enums_impl<enum_type>::from_underlying_try(value);
+    }
 
     /// --------------------------------------------------------------------------------------------
     /// returns enum value at index `index`.
     /// --------------------------------------------------------------------------------------------
     template <typename enum_type>
     constexpr auto from_index_unchecked(usize index) -> enum_type
-        requires is_enum<enum_type>;
+        requires is_enum<enum_type>
+    {
+        return _enums_impl<enum_type>::from_index_unchecked(index);
+    }
 
     /// --------------------------------------------------------------------------------------------
     /// returns enum value at index `index`.
     /// --------------------------------------------------------------------------------------------
     template <typename enum_type>
     constexpr auto from_index_try(usize index) -> option<enum_type>
-        requires is_enum<enum_type>;
+        requires is_enum<enum_type>
+    {
+        return _enums_impl<enum_type>::from_index_try(index);
+    }
 
     /// --------------------------------------------------------------------------------------------
     /// returns the name of the `enum_type`.
     /// --------------------------------------------------------------------------------------------
     template <typename enum_type>
-    consteval auto get_name() -> string_view
-        requires is_enum<enum_type>;
+    consteval auto get_type_name() -> string_view
+        requires is_enum<enum_type>
+    {
+        return _enums_impl<enum_type>::get_type_name();
+    }
 
     /// --------------------------------------------------------------------------------------------
     /// returns the count of enum values present in `enum_type`.
     /// --------------------------------------------------------------------------------------------
     template <typename enum_type>
     consteval auto get_count() -> usize
-        requires is_enum<enum_type>;
+        requires is_enum<enum_type>
+    {
+        return _enums_impl<enum_type>::get_count();
+    }
 
     /// --------------------------------------------------------------------------------------------
     /// returns an `array_view` of values.
     /// --------------------------------------------------------------------------------------------
     template <typename enum_type>
     consteval auto get_values() -> array_view<enum_type>
-        requires is_enum<enum_type>;
+        requires is_enum<enum_type>
+    {
+        return _enums_impl<enum_type>::get_values();
+    }
 
     /// --------------------------------------------------------------------------------------------
     /// returns an `array_view` of string representation of enum values.
     /// --------------------------------------------------------------------------------------------
     template <typename enum_type>
     consteval auto get_strings() -> array_view<string_view>
-        requires is_enum<enum_type>;
+        requires is_enum<enum_type>
+    {
+        return _enums_impl<enum_type>::get_strings();
+    }
 
     /// --------------------------------------------------------------------------------------------
     /// returns an `array_view` of pairs of enum values and their string representations.
     /// --------------------------------------------------------------------------------------------
     template <typename enum_type>
     consteval auto get_entries() -> array_view<tuple<enum_type, string_view>>
-        requires is_enum<enum_type>;
+        requires is_enum<enum_type>
+    {
+        return _enums_impl<enum_type>::get_entries();
+    }
 
     /// --------------------------------------------------------------------------------------------
     /// returns `true` if value `value` exists.
     /// --------------------------------------------------------------------------------------------
     template <typename enum_type>
     constexpr auto is_value_valid(enum_type value) -> bool
-        requires is_enum<enum_type>;
+        requires is_enum<enum_type>
+    {
+        return _enums_impl<enum_type>::is_value_valid(value);
+    }
 
     /// --------------------------------------------------------------------------------------------
     /// returns `true` if a value at index `index` exists.
     /// --------------------------------------------------------------------------------------------
     template <typename enum_type>
     constexpr auto is_index_valid(usize index) -> bool
-        requires is_enum<enum_type>;
+        requires is_enum<enum_type>
+    {
+        return _enums_impl<enum_type>::is_index_valid(index);
+    }
 
     /// --------------------------------------------------------------------------------------------
     ///  returns `true` if a value whose underlying representation is `value` exists.
     /// --------------------------------------------------------------------------------------------
     template <typename enum_type>
     constexpr auto is_underlying_valid(get_underlying_type<enum_type> value) -> bool
-        requires is_enum<enum_type>;
+        requires is_enum<enum_type>
+    {
+        return _enums_impl<enum_type>::is_underlying_valid(value);
+    }
 
     /// --------------------------------------------------------------------------------------------
     /// returns the index of the enum value if valid, else max.
     /// --------------------------------------------------------------------------------------------
     template <typename enum_type>
     constexpr auto to_index(enum_type value) -> usize
-        requires is_enum<enum_type>;
+        requires is_enum<enum_type>
+    {
+        return _enums_impl<enum_type>::to_index(value);
+    }
 
     /// --------------------------------------------------------------------------------------------
     /// returns the underlying representation of enum value.
     /// --------------------------------------------------------------------------------------------
     template <typename enum_type>
     constexpr auto to_underlying(enum_type value) -> get_underlying_type<enum_type>
-        requires is_enum<enum_type>;
+        requires is_enum<enum_type>
+    {
+        return _enums_impl<enum_type>::to_underlying(value);
+    }
 
     /// --------------------------------------------------------------------------------------------
-    /// returns the string representation of enum value.
+    /// returns the null terminated string representation of enum value if valid, else empty.
     /// --------------------------------------------------------------------------------------------
     template <typename enum_type>
     constexpr auto to_string_view(enum_type value) -> string_view
         requires is_enum<enum_type>
     {
-        return string_view::from_std(magic_enum::enum_name(value));
+        return _enums_impl<enum_type>::to_string_view(value);
     }
 
     /// --------------------------------------------------------------------------------------------
@@ -190,7 +234,10 @@ namespace atom::enums
     /// --------------------------------------------------------------------------------------------
     template <typename enum_type>
     consteval auto get_min() -> enum_type
-        requires is_enum<enum_type>;
+        requires is_enum<enum_type>
+    {
+        return _enums_impl<enum_type>::get_min();
+    }
 
     /// --------------------------------------------------------------------------------------------
     /// returns the enum value which has the maximum underlying type. in case of flags, returns
@@ -198,54 +245,102 @@ namespace atom::enums
     /// --------------------------------------------------------------------------------------------
     template <typename enum_type>
     consteval auto get_max() -> enum_type
-        requires is_enum<enum_type>;
+        requires is_enum<enum_type>
+    {
+        return _enums_impl<enum_type>::get_max();
+    }
 
     /// --------------------------------------------------------------------------------------------
     /// adds the rhs flags into `lhs` flags and returns the result.
     /// --------------------------------------------------------------------------------------------
     template <typename enum_type>
     constexpr auto add_flags(enum_type lhs, enum_type rhs) -> enum_type
-        requires is_enum<enum_type> and is_flags<enum_type>;
+        requires is_enum<enum_type> and is_flags<enum_type>
+    {
+        return _enums_impl<enum_type>::add_flags(lhs, rhs);
+    }
 
     /// --------------------------------------------------------------------------------------------
     /// removes the rhs flags from `lhs` flags and returns the result.
     /// --------------------------------------------------------------------------------------------
     template <typename enum_type>
     constexpr auto remove_flags(enum_type lhs, enum_type rhs) -> enum_type
-        requires is_enum<enum_type> and is_flags<enum_type>;
+        requires is_enum<enum_type> and is_flags<enum_type>
+    {
+        return _enums_impl<enum_type>::remove_flags(lhs, rhs);
+    }
 
     /// --------------------------------------------------------------------------------------------
     /// returns only the flags present in both `lhs` and `rhs`.
     /// --------------------------------------------------------------------------------------------
     template <typename enum_type>
     constexpr auto get_common_flags(enum_type lhs, enum_type rhs) -> enum_type
-        requires is_enum<enum_type> and is_flags<enum_type>;
+        requires is_enum<enum_type> and is_flags<enum_type>
+    {
+        return _enums_impl<enum_type>::get_common_flags(lhs, rhs);
+    }
 
     /// --------------------------------------------------------------------------------------------
     /// adds `lhs` and `rhs` and removes the flags that are common in both.
     /// --------------------------------------------------------------------------------------------
     template <typename enum_type>
     constexpr auto get_uncommon_flags(enum_type lhs, enum_type rhs) -> enum_type
-        requires is_enum<enum_type> and is_flags<enum_type>;
+        requires is_enum<enum_type> and is_flags<enum_type>
+    {
+        return _enums_impl<enum_type>::get_uncommon_flags(lhs, rhs);
+    }
 
     /// --------------------------------------------------------------------------------------------
     /// returns all the flags not present in both `flags`.
     /// --------------------------------------------------------------------------------------------
     template <typename enum_type>
-    constexpr auto reverse_flags(enum_type flags) -> enum_type
-        requires is_enum<enum_type> and is_flags<enum_type>;
+    constexpr auto get_reverse_flags(enum_type flags) -> enum_type
+        requires is_enum<enum_type> and is_flags<enum_type>
+    {
+        return _enums_impl<enum_type>::get_reverse_flags(flags);
+    }
 
     /// --------------------------------------------------------------------------------------------
     /// returns `true` if `lhs` has all the `rhs` flags.
     /// --------------------------------------------------------------------------------------------
     template <typename enum_type>
     constexpr auto has_all_flags(enum_type lhs, enum_type rhs) -> bool
-        requires is_enum<enum_type> and is_flags<enum_type>;
+        requires is_enum<enum_type> and is_flags<enum_type>
+    {
+        return _enums_impl<enum_type>::has_all_flags(lhs, rhs);
+    }
 
     /// --------------------------------------------------------------------------------------------
     /// returns `true` if `lhs` has any of the `rhs` flags.
     /// --------------------------------------------------------------------------------------------
     template <typename enum_type>
     constexpr auto has_any_flags(enum_type lhs, enum_type rhs) -> bool
-        requires is_enum<enum_type> and is_flags<enum_type>;
+        requires is_enum<enum_type> and is_flags<enum_type>
+    {
+        return _enums_impl<enum_type>::has_any_flags(lhs, rhs);
+    }
+}
+
+namespace atom
+{
+    template <typename enum_type>
+    constexpr auto operator|(enum_type lhs, enum_type rhs) -> enum_type
+        requires enums::is_enum<enum_type> and enums::is_flags<enum_type>
+    {
+        return _enums_impl<enum_type>::add_flags(lhs, rhs);
+    }
+
+    template <typename enum_type>
+    constexpr auto operator&(enum_type lhs, enum_type rhs) -> enum_type
+        requires enums::is_enum<enum_type> and enums::is_flags<enum_type>
+    {
+        return _enums_impl<enum_type>::get_common_flags(lhs, rhs);
+    }
+
+    template <typename enum_type>
+    constexpr auto operator^(enum_type lhs, enum_type rhs) -> enum_type
+        requires enums::is_enum<enum_type> and enums::is_flags<enum_type>
+    {
+        return _enums_impl<enum_type>::get_uncommon_flags(lhs, rhs);
+    }
 }
