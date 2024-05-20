83b64e9388cd9ac569e187bda39f61269b47722a
shifu-dev <csingh8476@gmail.com>
2024-05-20 16:32:54 +0530

    update: add cmake option for testing.

--- cmake/atom_core-config.cmake
+++ cmake/atom_core-config.cmake
@@ -2,6 +2,5 @@ include(CMakeFindDependencyMacro)
 
 find_dependency(fmt REQUIRED)
 find_dependency(magic_enum REQUIRED)
-find_dependency(Catch2 REQUIRED)
 
 include("${CMAKE_CURRENT_LIST_DIR}/atom_core-targets.cmake")
