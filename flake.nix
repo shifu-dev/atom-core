80193ac401c202ed275a723ba612ece234057476
shifu-dev <csingh8476@gmail.com>
2024-05-23 02:32:02 +0530

    update: expose flake config options.

--- flake.nix
+++ flake.nix
@@ -53,22 +53,11 @@
                 cmake --install build --prefix $out;
             '';
         };
+    in rec
+    {
+        env.${system}.default = rec {
 
-        clang_scan_deps_include_paths = lib.strings.concatStrings [
-            " -I /nix/store/csml9b5w7z51yc7hxgd2ax4m6vj36iyq-libcxx-18.1.5-dev/include"
-            " -I /nix/store/2sf9x4kf8lihldhnhp2b8q3ybas3p83l-compiler-rt-libc-18.1.5-dev/include"
-            " -I /nix/store/hrssqr2jypca2qcqyy1xmfdw71nv6n14-catch2-3.5.2/include"
-            " -I /nix/store/zc8xnz48ca61zjplxc3zz1ha3zss046p-fmt-10.2.1-dev/include"
-            " -I /nix/store/2j35qpxbprdgcixyg70lyy6m0yay9352-magic-enum-0.9.5/include"
-            " -I /nix/store/k3701zl6gmx3la7y4dnflcvf3xfy88kh-python3-3.11.9/include"
-            " -I /nix/store/csml9b5w7z51yc7hxgd2ax4m6vj36iyq-libcxx-18.1.5-dev/include/c++/v1"
-            " -I /nix/store/fymdqlxx6zsqvlmfwls3h2fly9kz0vcf-clang-wrapper-18.1.5/resource-root/include"
-            " -I /nix/store/s3pvsv4as7mc8i2nwnk2hnsyi2qdj4bq-glibc-2.39-31-dev/include"
-        ];
-
-        derivation = stdenv.mkDerivation {
-
-            name = "atom.core";
+            name = "atom-core";
 
             src = ./.;
 
@@ -102,17 +91,41 @@
                 cmake --install build --prefix $out;
             '';
 
-            CXXFLAGS = clang_scan_deps_include_paths;
-            CMAKE_GENERATOR = "Ninja";
-            CMAKE_BUILD_TYPE = "Debug";
-            CMAKE_EXPORT_COMPILE_COMMANDS = "true";
+            clang_scan_deps_include_paths = [
+                "/nix/store/csml9b5w7z51yc7hxgd2ax4m6vj36iyq-libcxx-18.1.5-dev/include"
+                "/nix/store/2sf9x4kf8lihldhnhp2b8q3ybas3p83l-compiler-rt-libc-18.1.5-dev/include"
+                "/nix/store/hrssqr2jypca2qcqyy1xmfdw71nv6n14-catch2-3.5.2/include"
+                "/nix/store/zc8xnz48ca61zjplxc3zz1ha3zss046p-fmt-10.2.1-dev/include"
+                "/nix/store/2j35qpxbprdgcixyg70lyy6m0yay9352-magic-enum-0.9.5/include"
+                "/nix/store/k3701zl6gmx3la7y4dnflcvf3xfy88kh-python3-3.11.9/include"
+                "/nix/store/csml9b5w7z51yc7hxgd2ax4m6vj36iyq-libcxx-18.1.5-dev/include/c++/v1"
+                "/nix/store/fymdqlxx6zsqvlmfwls3h2fly9kz0vcf-clang-wrapper-18.1.5/resource-root/include"
+                "/nix/store/s3pvsv4as7mc8i2nwnk2hnsyi2qdj4bq-glibc-2.39-31-dev/include"
+            ];
+
+            envVars = {
+                CXXFLAGS = lib.strings.concatMapStrings (v: " -I " + v) clang_scan_deps_include_paths;
+                CMAKE_GENERATOR = "Ninja";
+                CMAKE_BUILD_TYPE = "Debug";
+                CMAKE_EXPORT_COMPILE_COMMANDS = "true";
+            };
         };
-    in
-    {
-        inherit clang_scan_deps_include_paths;
 
-        devShells.${system}.default = derivation;
+        devShells.${system}.default = with env.${system}.default; stdenv.mkDerivation ({
+            inherit name;
+            inherit src;
+            inherit propagatedBuildInputs;
+            inherit nativeBuildInputs;
+        } // envVars);
 
-        packages.${system}.default = derivation;
+        packages.${system}.default = with env.${system}.default; stdenv.mkDerivation ({
+            inherit name;
+            inherit src;
+            inherit propagatedBuildInputs;
+            inherit nativeBuildInputs;
+            inherit configurePhase;
+            inherit buildPhase;
+            inherit installPhase;
+        } // envVars);
     };
 }
