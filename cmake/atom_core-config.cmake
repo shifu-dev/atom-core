f5b02670eac660c9e2c0fc01314ee8be20a238da
shifu-dev <csingh8476@gmail.com>
2024-05-26 14:06:42 +0530

    update: add missing dependency to config.

--- cmake/atom_core-config.cmake
+++ cmake/atom_core-config.cmake
@@ -2,5 +2,6 @@ include(CMakeFindDependencyMacro)
 
 find_dependency(fmt REQUIRED)
 find_dependency(magic_enum REQUIRED)
+find_dependency(cpptrace REQUIRED)
 
 include("${CMAKE_CURRENT_LIST_DIR}/atom_core-targets.cmake")
